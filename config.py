d18d584e1746f9f0bd158a44e170ea9c55ed3985
garryaklia31 <158801619+garryaklia31@users.noreply.github.com>
2025-05-06 16:04:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ from os import environ
 API_ID = int(environ.get("API_ID", "22182189"))
 API_HASH = environ.get("API_HASH", "5e7c4088f8e23d0ab61e29ae11960bf5")
 BOT_TOKEN = environ.get("BOT_TOKEN", "8146719460:AAGwfW09HZ1j94OY3M-4BzHrcJysJsgpbS0")
-VIP_USER = os.environ.get('VIP_USERS', '1718738592').split(',')
+VIP_USER = environ.get('VIP_USERS', '1718738592').split(',')
 VIP_USERS = [int() for user_id in VIP_USER]
